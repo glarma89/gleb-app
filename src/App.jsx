64613b6bf462058411b99ae196b9295397e87252
Gleb Artiushin <glarma89@gmail.com>
2025-12-02 19:15:07 +0200

    fourth commit write

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Smite and React logos to learn more
+        Click on the Write and React logos to learn more
       </p>
     </>
   )
